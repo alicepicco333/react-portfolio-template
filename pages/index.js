312b8f19420bf735d4e9a9e23042c8d494105032
ALICE <alice.picco@studio.unibo.it>
2024-04-25 14:22:49 +0200

    upppp

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ const Home = () => {
           <meta name="description" content="My portfolio website" />
           <meta property="og:title" content="Alice Picco - portfolio" />
           <meta property="og:description" content="This website contains all my latest projects as well as my resume and contact information." />
-          <meta property="og:image" content="https://i.ibb.co/hsDhr5v/Screenshot-2024-04-25-140857.png" />
+          <meta property="og:image" content="https://i.ibb.co/phST7kC/Screenshot-2024-04-25-140857.png" />
           <meta property="og:url" content="https://alicexpicco.netlify.app" />
           <meta property="og:type" content="website" />
           <link rel="icon" href="/favicon.ico" />
