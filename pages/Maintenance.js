b8228884260b516e219f785b19330c7a7ef6d09d
Alice Picco <alice.picco@studio.unibo.it>
2025-06-13 12:12:29 +0200

    commit

---
+++ pages/Maintenance.js
@@ -0,0 +1,11 @@
+// src/Maintenance.js
+import React from "react";
+
+const Maintenance = () => (
+  <div className="h-screen flex flex-col items-center justify-center bg-[#f8fafc] text-center p-6">
+    <h1 className="text-4xl font-bold text-gray-800 mb-4">currently under maintenance</h1>
+    <p className="text-lg text-gray-600">everything is always changing</p>
+  </div>
+);
+
+export default Maintenance;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
-const App = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-};
+// const App = ({ Component, pageProps }) => {
+  // return (
+    // <ThemeProvider>
+     // <Component {...pageProps} />
+   //  </ThemeProvider>
+//  );
+// };
+
+// src/App.js
+
+import Maintenance from "./Maintenance";
+
+function App() {
+  return <Maintenance />;
+}
 
 export default App;
